34df7e05e9d2b34cd53eb59af99c44068acf820f
erick <124009200+TambukinErick@users.noreply.github.com>
2025-02-18 18:08:53 +0800

    Add grid functionality and styling

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+let numOfTilesPerSide = 16;
+const container = document.querySelector("#container");
 
+const containerSize = container.offsetHeight;
+console.log(containerSize);
+function populateContainer(numOfTilesPerSide) {
+  if (numOfTilesPerSide > 100 || numOfTilesPerSide < 0) {
+    return;
+  }
+
+  const tileSize = Math.floor(
+    containerSize/numOfTilesPerSide
+  ) - 1;
+  console.log(tileSize);
+  
+  for (let i = 0; i < numOfTilesPerSide**2; i++){
+    const tile = document.createElement("div");
+    tile.style.height = `${tileSize}px`;
+    tile.style.width = `${tileSize}px`;
+    tile.style.borderStyle = "solid";
+    tile.style.borderColor = "black";
+    tile.style.borderWidth = "1px";
+    container.appendChild(tile);
+  }
+}
+
+populateContainer(numOfTilesPerSide);
